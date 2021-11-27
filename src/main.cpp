fb68a93bffd3abd8632a2f3b32f8fd418a43d562
-=Oktan=- <r.zhigalenko@mail.ru>
2021-11-27 18:09:25 +0500

    add layuot

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,19 @@
 //#include <QSlider>
 //#include <QSpinBox>
 #include <iostream>
-#include <QTimer>
+//#include <QTimer>
 //#include "print.h"
 #include <QPushButton>
-#include <QPixmap>
-#include <QPaintEvent>
-#include <QPainter>
+//#include <QPixmap>
+//#include <QPaintEvent>
+//#include <QPainter>
 
-class ImageButton : public QPushButton
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+
+
+/*class ImageButton : public QPushButton
 {
     Q_OBJECT
 
@@ -82,12 +87,12 @@ void ImageButton::setUp()
 {
     mCurrentButtonPixmap = mButtonUpPixmap;
     update();
-}
+}*/
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
-    
+    //----Spinbox and slider----------------
    /* auto *slider = new QSlider(Qt::Horizontal);
 
     auto *spinbox = new QSpinBox();
@@ -109,11 +114,35 @@ int main(int argc, char **argv)
 
     slider->show();
     spinbox->show();*/
-    ImageButton redButton(nullptr);
+    //----------------------------------------
+   //------------Image--------------
+    /*ImageButton redButton(nullptr);
     redButton.setFixedSize(100, 100);
     redButton.move(1000, 400);
     QObject::connect(&redButton, &QPushButton::clicked, [](){std::cout << "cliked\n";});
-    redButton.show();
+    redButton.show();*/
+    //------------------------------
+
+    QWidget *window = new QWidget;
+    QPushButton *button1 = new QPushButton("One", window);
+    QPushButton *button2 = new QPushButton("Two", window);
+    QPushButton *button3 = new QPushButton("Three", window);
+    QPushButton *button4 = new QPushButton("Four", window);
+    QPushButton *button5 = new QPushButton("Five", window);
+    
+    auto *layout  = new QGridLayout(window);
+    layout->addWidget(button1, 0, 0);
+    layout->addWidget(button2, 0, 1);
+    layout->addWidget(button3, 1, 0, 1, 2);
+    layout->addWidget(button4, 2, 0);
+    layout->addWidget(button5, 2, 1);
+  //  button1->move(40, 50);
+  //  button2->move(140, 150);
+  //  button3->move(200, 200);
+
+    window->resize(300, 300);
+    window->move(1000, 500);
+    window->show();
     return app.exec();
 }
 #include "main.moc"
